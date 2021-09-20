dca0c3cfd3dd209a973a37d2140c3eb1651c1542
Punit Jain <jainpunit7000@gmail.com>
2021-09-20 09:50:14 +0530

    Connection to MONGODB done

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser');
 // creating Server 
 const app = express() ;
 
+// Mongo DB URI Connector
+const MONGO_DB_URI = "mongodb+srv://pjain:DyMpOzlue13h13xP@cluster0.gxnuq.mongodb.net/users"
+
 //configuring body-parser for Api Requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
@@ -19,7 +22,16 @@ app.use("/",(req,res,next) => {
     })
 })
 
-// Running the Server
-app.listen(5000,() => {
-    console.log("Server is Runnning at port 5000") ;
-})
+
+// Running the Server after connecting to MONGODB
+mongoose.connect(MONGO_DB_URI)
+    .then( conn => {
+        console.log(":: app connected to DB") ;
+        app.listen(5000,() => {
+            console.log(":: Server is Runnning at port 5000") ;
+        })
+    } )
+    .catch( err => {
+        console.log(":: Connection to DB failed") ;
+        console.log(err);
+    } )
